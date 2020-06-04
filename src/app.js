282d2fdf21d920f0360d154e81dbae23ded5df6f
RemyMach <rmachavoine@wynd.eu>
2020-06-04 10:58:04 +0200

    liaison fichier js, CSS, img

--- src/app.js
+++ src/app.js
@@ -2,30 +2,11 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public/HTML')
+const publicDirectoryPath = path.join(__dirname, '../public/')
 
 // on utilise express.static pour rendre des fichiers static (CSS, JS, HTML...)
 app.use('/',express.static(publicDirectoryPath))
 // on peut avoir accès à about.html et help.html en tapant le nom complet du coup
-
-// app.com
-// app.com/help
-// app.com/about
-
-// app.get('/', (req, res) => {
-//     res.send('<h1>Hello Home</h1>')
-// })
-
-// app.get('/help', (req, res) => {
-//     res.send({
-//         name: 'tom',
-//         age: 24
-//     })
-// })
-
-// app.get('/about', (req, res) => {
-//     res.send('<h1>Hello about</h1>')
-// })
 
 app.get('/weather', (req, res) => {
     res.send({
