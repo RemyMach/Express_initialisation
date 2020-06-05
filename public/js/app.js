b7fc6fc820b138121b86e9ca15cc9db9e6bf3b8b
RemyMach <rmachavoine@wynd.eu>
2020-06-05 12:50:06 +0200

    permet de ne pas submit un formulaire

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,6 @@
 //on utilise l'api de fetch qui n'est accessible que des navigateurs
 console.log('je suis dans le côté client')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
 fetch('http://localhost:3000/weather?adress=Boston').then((response) => {
     response.json().then((data) => {
         if(data.error){
@@ -15,4 +9,13 @@ fetch('http://localhost:3000/weather?adress=Boston').then((response) => {
             console.log(data)
         }
     })
+})
+
+const weatherForm = document.querySelector('form')
+console.log(weatherForm)
+weatherForm.addEventListener('submit', (e) => {
+    //permet de ne pas submit une page
+    console.log(e)
+    e.preventDefault()
+    console.log('testing')
 })
